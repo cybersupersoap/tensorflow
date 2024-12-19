a4f1303425b8ecafad73d65f2e151228beeeed4d
Assoap <112180992+Assoap@users.noreply.github.com>
2024-12-19 16:29:02 +0800

    Fix crash of MapUnstage

--- tensorflow/core/kernels/map_stage_op.cc
+++ tensorflow/core/kernels/map_stage_op.cc
@@ -588,6 +588,14 @@ class MapUnstageOp : public OpKernel {
 
     OP_REQUIRES_OK(ctx, ctx->input("key", &key_tensor));
     OP_REQUIRES_OK(ctx, ctx->input("indices", &indices_tensor));
+    OP_REQUIRES(ctx, key_tensor->NumElements() > 0,
+                errors::InvalidArgument("key must not be empty"));
+
+    OP_REQUIRES(ctx, key_tensor->NumElements() == 1,
+                errors::InvalidArgument(
+                    "key must be an int64 scalar, got tensor with shape: ",
+                    key_tensor->shape()));
+
     OP_REQUIRES_OK(ctx, map->pop(key_tensor, indices_tensor, &tuple));
 
     OP_REQUIRES(
